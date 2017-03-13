264e069aed5bd3532079dee0987c986e8ea550da
gaysina-elizaveta <gaysina.elizaveta@gmail.com>
2017-03-13 19:17:14 +0500

    1

---
+++ Pyatnashki-1/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyatnashki_1
+{
+    class Game
+    {
+        private int[,] square;
+        int? x;
+        int? y;
+
+       
+
+       
+}
+}
